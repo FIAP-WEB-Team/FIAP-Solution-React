7f55e184003718de14d85722fe13192bcb11dba2
pedro <fabrica.software.ltda@outlook.com>
2023-01-11 18:17:21 -0300

    Passenger forms atualization

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,10 +1,10 @@
 import './Button.css';
 
 const Button = (props) => {
-    const css = {background: props.background, color: props.color, display: props.display};
+    const css = { background: props.background, color: props.color, display: props.display };
 
     return (
-        <button type={props.type} className='button-custom' id={props.id} style={css} disabled={props.disabled} >{props.label}</button>
+        <button type={props.type} className='button-custom' id={props.id} style={css} disabled={props.disabled}>{props.label}</button>
     )
 }
 
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,14 +2,30 @@ import './Form.css';
 import TripSelection from '../TripSelection';
 import Button from '../Button';
 import FlightSelection from '../FlightSelection';
+import styles from './FormConfigsStyle.module.css';
+import { useEffect, useState } from "react";
 
 function Form() {
+    const [CheckYES, setCheckYES] = useState(true);
+    const [CheckNO, setCheckNO] = useState(false);
+    const [dadosUserPadrao, setdadosUserPadrao] = useState({ firstName: 'Giba', secondName: 'Dos Santos', birthDate: '1997-10-11', nacionality: 'Brasileiro', gender: 'Masculino' });
+
+    useEffect(() => {
+        if (CheckYES == true) {
+            setdadosUserPadrao({ firstName: 'Giba', secondName: 'Dos Santos', birthDate: '1997-10-11', nacionality: 'Brasileiro', gender: 'Masculino' });
+            setCheckNO(false)
+        }
+        else {
+            setdadosUserPadrao({ firstName: '', secondName: '', birthDate: '', nacionality: '', gender: '' });
+            setCheckNO(true);
+        }
+    }, [CheckYES])
 
     return (
         <div className="container">
             <h2>Com a GOL, você entra em 2023 voando! </h2>
             <form method="POST" id="flight-form" className="flight-form">
-                
+
                 <div className="steps clearfix">
                     <ul role="tablist">
                         <li role="tab" className="flight-form-step-li first" cod="1">
@@ -20,7 +36,7 @@ function Form() {
                                     <span className="title_text">Escolha a viagem</span>
                                 </div>
                             </a>
-                        </li>                    
+                        </li>
                         <li role="tab" className="flight-form-step-li disabled" cod="2">
                             <a className="flight-form-step-a" href="#" >
                                 <div className="title">
@@ -46,36 +62,78 @@ function Form() {
                     <TripSelection />
                     <FlightSelection />
 
-                    <fieldset id="flight-form-p-3" role="tabpanel" className="flight_fieldset_aba body current" cod="3" style={{display: 'none'}}>
-                        A DESENVOLVER...
+                    <fieldset id="flight-form-p-3" role="tabpanel" className="flight_fieldset_aba body current" cod="3" style={{ display: 'none' }}>
+                        <div className={styles.divForm}>
+                            <h2 className={styles.primaltitle}>Preencha os dados do Passageiro</h2>
+                            <h2 className={styles.secondtitle}> Giba, voce é o passageiro dessa viagem?</h2>
+                            <div className={styles.divcheck}>
+                                <label className={styles.primaltext}> sim</label>
+
+                                <label className={styles.checkboxstyle}>
+                                    <input type='checkbox' className={styles.primalcheckbox} defaultChecked={CheckYES} checked={CheckYES} onChange={() => setCheckYES(!CheckYES)}></input>
+                                </label>
+
+                                <label className={styles.secondtext}> não</label>
+                                <label className={styles.checkboxstyle}>
+                                    <input className={styles.secondcheckbox} type='checkbox' defaultChecked={CheckNO} checked={CheckNO} onChange={() => setCheckYES(!CheckYES)}></input>
+                                </label>
+
+                            </div>
+                        </div>
+                        <div >
+                            <form>
+                                <div className={styles.formsCorner}>
+                                    <p>PRIMEIRO NOME: </p>
+                                    <input type='text' placeholder="Digite seu nome Aqui" className={styles.inputstyle} name='tbFirstName' value={dadosUserPadrao.firstName} onChange={e => setdadosUserPadrao({ ...dadosUserPadrao, firstName: e.target.value })}></input>
+
+                                </div>
+                                <div className={styles.formsCorner}>
+                                    <p> SOBRENOME:</p>
+                                    <input type='text' placeholder="Digite seu Sobrenome Aqui" className={styles.inputstyle} name='tbLastName' value={dadosUserPadrao.secondName} onChange={e => setdadosUserPadrao({ ...dadosUserPadrao, secondName: e.target.value })}></input>
+                                </div>
+                                <div className={styles.formsCorner}>
+                                    <p> DATA DE NASCIMENTO:</p>
+                                    <input type='date' name='tbBirthName' className={styles.datepicker} placeholder='dd/mm/aaaa' value={dadosUserPadrao.birthDate} onChange={e => setdadosUserPadrao({ ...dadosUserPadrao, birthDate: e.target.value })}></input>
+                                </div>
+                                <div className={styles.formsCorner}>
+                                    <p> NACIONALIDADE:</p>
+                                    <input type='text' placeholder="Digite sua nacionalidade" className={styles.inputstyle} name='tbNacionality' value={dadosUserPadrao.nacionality} onChange={e => setdadosUserPadrao({ ...dadosUserPadrao, nacionality: e.target.value })}></input>
+                                </div>
+                                <div className={styles.formsCorner}>
+                                    <p> GÊNERO:</p>
+                                    <input type='text' placeholder="não é obrigatório" className={styles.inputstyle} name='tbGender' value={dadosUserPadrao.gender} onChange={e => setdadosUserPadrao({ ...dadosUserPadrao, gender: e.target.value })}></input>
+                                </div>
+                            </form>
+                        </div >
                     </fieldset>
 
                 </div>
 
                 <div className="fieldset-footer">
-                    <span id="form-step-span" step="1">Step 1 of 3</span>                    
-                    <Button 
-                        type="button" 
-                        label="Proximo" 
+                    <span id="form-step-span" step="1">Step 1 of 3</span>
+                    <Button
+                        type="button"
+                        label="Proximo"
                         id="btnNext"
                         disabled={false}
-                        background="#1ABC9C"
-                        color="#FFF" 
+                        background="#F24E1E"
+                        color="#FFF"
                         display=""
+
                     />
-                    <Button 
+                    <Button
                         type="button"
-                        label="Anterior" 
+                        label="Anterior"
                         id="btnPrevious"
                         disabled={false}
-                        background="#DEDEDE"
-                        color="#646464"
-                        display="none"  
+                        background="#F24E1E"
+                        color="#FFF"
+                        display="none"
                     />
                 </div>
 
-            </form>
-        </div>
+            </form >
+        </div >
     )
 }
 
